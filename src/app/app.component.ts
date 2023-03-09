3f0bf6f56ab4b7e43b0644c37b27097da287ed01
Necdet UYGUR <necdet.uygur@gmail.com>
2023-03-09 17:58:58 +0300

    Capacitor Reklam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { GameService } from './services/game.service';
+import {
+  AdMob,
+  BannerAdOptions,
+  BannerAdSize,
+  BannerAdPosition,
+} from '@capacitor-community/admob';
+import { Device } from '@capacitor/device';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +15,25 @@ import { GameService } from './services/game.service';
 })
 export class AppComponent {
   constructor(public gameService: GameService) {}
+
+  async ngOnInit() {
+    const deviceInfo = await Device.getInfo();
+    if (deviceInfo.platform !== 'web') {
+      AdMob.initialize({
+        requestTrackingAuthorization: true,
+        testingDevices: [''],
+        initializeForTesting: true,
+      });
+
+      const admobOptions: BannerAdOptions = {
+        adId: 'ca-app-pub-3940256099942544~3347511713',
+        adSize: BannerAdSize.FULL_BANNER,
+        position: BannerAdPosition.TOP_CENTER,
+        margin: 0,
+        isTesting: true,
+        // npa: true
+      };
+      AdMob.showBanner(admobOptions);
+    }
+  }
 }
