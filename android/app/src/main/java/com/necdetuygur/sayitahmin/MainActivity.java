fb6f63980c7ecbbce88f12956f7dea2e9992942d
Necdet UYGUR <necdet.uygur@gmail.com>
2023-03-09 14:40:39 +0300

    Mobile application

---
+++ android/app/src/main/java/com/necdetuygur/sayitahmin/MainActivity.java
@@ -0,0 +1,5 @@
+package com.necdetuygur.sayitahmin;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.necdetuygur.sayitahmin',
+  appName: 'Sayı Tahmin Oyunu',
+  webDir: 'dist/ng-sayitahmin',
+  bundledWebRuntime: false
+};
+
+export default config;
