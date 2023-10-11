8cb9b01350c8854c1e4bc201cd6dfa0328a28b00
Necdet UYGUR <necdet.uygur@gmail.com>
2023-10-11 14:55:00 +0300

    enableProdMode

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+enableProdMode();
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
